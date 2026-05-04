207a92f330df4442ddf56cd6a0f6e31ba6b39345
Persibaldo <percivalgonzalez12@gmail.com>
2026-05-04 21:15:18 +0200

    Fix HTML formatting in index.php

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
-<!DOCTYPE html >
-<html lang=”es” >
+<!DOCTYPE html>
+<html lang="es">
 <head>
-<meta charset = "UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="author" content="Percival">
-<title> Index </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Percival">
+    <title>Index</title>
 </head>
 <body>
-    <!-- index.php -->
-<form action="insert.php" method="GET">
-    <label>¿Qué desea consultar?</label>
-    <select name="tabla">
-        <option value="usuarios">Usuarios</option>
-        <option value="series">Series</option>
-    </select>
-    <button type="submit">Continuar</button>
-</form>
-
-<?php
+    <form action="insert.php" method="GET">
+        <label>¿Qué desea consultar?</label>
+        <select name="tabla">
+            <option value="usuarios">Usuarios</option>
+            <option value="series">Series</option>
+        </select>
+        <button type="submit">Continuar</button>
+    </form>
+</body>
+</html>

481b557554cd3ffdca2d1e37a1ba75b17b64af98
Persibaldo <percivalgonzalez12@gmail.com>
2026-05-23 15:54:37 +0200

    Refactor biblioteca_total view to include genre names

--- vista.sql
+++ vista.sql
@@ -1,10 +1,22 @@
-CREATE OR REPLACE VIEW biblioteca_total AS
-SELECT id_genero, titulo, 'Película' AS tipo FROM pelis p JOIN peli_generos pg ON p.id_peli = pg.id_peli
-UNION ALL
-SELECT id_genero, titulo, 'Serie' AS tipo FROM series s JOIN serie_generos sg ON s.id_serie = sg.id_serie
-UNION ALL
-SELECT id_genero, titulo, 'Anime' AS tipo FROM anime a JOIN anime_generos ag ON a.id_anime = ag.id_anime
-UNION ALL
-SELECT id_genero, titulo, 'Libro' AS tipo FROM libros l JOIN libro_generos lg ON l.id_libro = lg.id_libro
-UNION ALL
-SELECT id_genero, titulo, 'Juego' AS tipo FROM juegos j JOIN juego_generos jg ON j.id_juego = jg.id_juego;
+CREATE OR REPLACE VIEW biblioteca_total AS 
+              SELECT b.titulo, b.tipo, GROUP_CONCAT(g.nombre_genero SEPARATOR ', ') as nombre_genero
+              FROM (
+                  SELECT titulo, 'Peli' as tipo, id_peli as id_item FROM pelis
+                  UNION ALL SELECT titulo, 'Serie', id_serie FROM series
+                  UNION ALL SELECT titulo, 'Anime', id_anime FROM anime
+                  UNION ALL SELECT titulo, 'Libro', id_libro FROM libros
+                  UNION ALL SELECT titulo, 'Juego', id_juego FROM juegos
+              ) b
+              LEFT JOIN peli_generos pg ON (b.tipo = 'Peli' AND b.id_item = pg.id_peli)
+              LEFT JOIN serie_generos sg ON (b.tipo = 'Serie' AND b.id_item = sg.id_serie)
+              LEFT JOIN anime_generos ag ON (b.tipo = 'Anime' AND b.id_item = ag.id_anime)
+              LEFT JOIN libro_generos lg ON (b.tipo = 'Libro' AND b.id_item = lg.id_libro)
+              LEFT JOIN juego_generos jg ON (b.tipo = 'Juego' AND b.id_item = jg.id_juego)
+              LEFT JOIN generos g ON (
+                  g.id_genero = pg.id_genero OR 
+                  g.id_genero = sg.id_genero OR 
+                  g.id_genero = ag.id_genero OR 
+                  g.id_genero = lg.id_genero OR 
+                  g.id_genero = jg.id_genero
+              )
+              GROUP BY b.tipo, b.titulo;
